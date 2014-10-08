2cd6fae89e56139cb6cb5b24e014560c51638756
WalrusCow <willgmcd@gmail.com>
2014-10-08 11:51:36 -0400

    Fix click coordinates in FF/Chrome

--- util.js
+++ util.js
@@ -24,15 +24,11 @@ define([], function() {
 
     var el = clickElem;
     do {
-      console.log(el, el.offsetLeft, el.scrollLeft, el.offsetTop, el.scrollTop);
       offset.x += el.offsetLeft - el.scrollLeft;
       offset.y += el.offsetTop - el.scrollTop;
-    } while (el = el.offsetParent)
+      // Do not do body, because chrome adds scroll to body but FF does not
+    } while ((el = el.offsetParent) && (el !== document.body))
 
-    // TODO: Verify offset and pageX/pageY in chrome/ff
-    //var x = e.clientX - offset.x;
-    //var y = e.clientY - offset.y;
-    console.log(e);
     var x = e.pageX - offset.x;
     var y = e.pageY - offset.y;
 
